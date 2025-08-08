5a32ba4248142d98bb7a7f13a571b4eb2f372955
Colin Ding <colinding10@gmail.com>
2025-08-08 15:08:13 -0400

    fix github link

--- src/consts.ts
+++ src/consts.ts
@@ -28,7 +28,7 @@ export const NAV_LINKS: SocialLink[] = [
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/cold',
+    href: 'https://github.com/colding10',
     label: 'GitHub',
   },
   {
