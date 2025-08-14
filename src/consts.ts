091e3f71c3431e7bd6b73737c44b1cb08b0337f8
Colin Ding <colinding10@gmail.com>
2025-08-13 23:39:41 -0400

    Initial commit with local changes

---
+++ src/consts.ts
@@ -0,0 +1,47 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'colin (cold)',
+  description:
+    'some very cold ctf writeups and other stuff',
+  href: 'https://cold.is-a.dev',
+  author: 'cold',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+  {
+    href: '/tags',
+    label: 'tags',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/colding10',
+    label: 'GitHub',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
